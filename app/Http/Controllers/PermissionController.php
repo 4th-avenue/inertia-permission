0486c72208b54db7ec2bf5aa1b662924cfe93a75
4th-avenue <quebec706@gmail.com>
2024-01-23 13:58:09 +0900

    권한(Permission) 만들기 페이지 생성

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -23,9 +23,9 @@ class PermissionController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render('Admin/Permissions/Create');
     }
 
     /**
